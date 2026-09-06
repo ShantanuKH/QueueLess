520c1093259462163e6645354750c5a34f4a3bb5
ShantanuKH <khadseshantanu02@gmail.com>
2026-09-06 12:49:23 +0530

    Allow production frontend CORS

--- backend/src/main/java/com/queueless/common/config/SecurityConfig.java
+++ backend/src/main/java/com/queueless/common/config/SecurityConfig.java
@@ -37,7 +37,10 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://queueless-1-ytne.onrender.com"
+                )
         );
 
         configuration.setAllowedMethods(
